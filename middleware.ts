fa15cfe359afb4f79b4fb5a39c08a6878760fc66
Samit <samitdas2003official@gmail.com>
2024-04-05 00:06:35 +0530

    deployment commit 5

--- middleware.ts
+++ middleware.ts
@@ -8,20 +8,20 @@ export default authMiddleware({
     "/api/webhook/clerk",
     "/api/webhook/stripe",
     "/api/uploadthing",
-    "assets/images/logo.svg",
+    "/assets/images/logo.svg",
     "/assets/images/hero.png",
     "/assets/images/dotted-pattern.png",
-    // "/sign-in"
+    "/sign-in"
   ],
   ignoredRoutes: [
     "/favicon.ico",
     "/api/webhook/clerk",
     "/api/webhook/stripe",
     "/api/uploadthing",
-    "assets/images/logo.svg",
+    "/assets/images/logo.svg",
     "/assets/images/hero.png",
     "/assets/images/dotted-pattern.png",
-    // "/sign-in"
+    "/sign-in"
   ]
 });
 
